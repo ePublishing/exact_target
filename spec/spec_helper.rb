26272277eef87da7386c5f331e9403920ab9448a
David McCullars <dmccullars@ePublishing.com>
2011-05-12 22:07:06 -0400

    Add support for simplecov-rcov for jenkins

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@ require 'rubygems'
 
 unless RUBY_VERSION.to_f < 1.9
   require 'simplecov'
-  SimpleCov.start do
-    add_filter '/spec/'
-    add_group 'Libraries', 'lib'
+  begin
+    require 'simplecov-rcov' # Formats in rcov format (so jenkins can consume)
+    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  rescue Exception => e
+    # Ignore if it isn't installed
   end
+  SimpleCov.start
 end
 
 require 'exact_target'
